d7356ed8bbfc211458026df1feaaec2e0b4df532
Gabriele Scalici <gaba41@gmail.com>
2021-03-18 11:36:52 +0100

    adding sma7

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import { readRemoteFile } from "react-papaparse";
 import { createChart } from "lightweight-charts";
 import Footer from './components/Footer';
 
-var chart = createChart(document.body, {
-  width: 1024, 
-  height: 300,
+document.body.style.position = 'relative';
+var container = document.createElement('div');
+document.body.appendChild(container);
+
+var width = 1024;
+var height = 300;
+
+var chart = createChart(container, {
+  width: width, 
+  height: height,
   layout: {
 		textColor: '#000',
 		backgroundColor: '#fff',
@@ -16,6 +23,7 @@ var areaSeries = chart.addAreaSeries({
   bottomColor: 'rgba(38, 198, 218, 0.04)',
   lineColor: 'rgba(38, 198, 218, 1)',
   lineWidth: 2,
+  visible: true,
 });
 
 let dayData = [];
@@ -32,29 +40,61 @@ readRemoteFile("./1.csv", {
       }
     });
     areaSeries.setData(dayData);
+    
+    //START SMA
+    var smaData = calculateSMA(dayData, 7);
+    var smaLine = chart.addAreaSeries({
+      color: 'rgba(4, 111, 232, 1)',
+      lineWidth: 2,
+      offset: 5,
+      visible: true,
+    });
+    smaLine.setData(smaData);
+    setMAText(smaData[smaData.length - 1].value);
+    chart.subscribeCrosshairMove((param) => {
+      setMAText(param.seriesPrices.get(smaLine));
+    });
+    //END SMA
   },
 });
 
-document.body.style.position = 'relative';
+var smaData = calculateSMA(dayData);
+var smaLine = chart.addAreaSeries();
+smaLine.setData(smaData);
 
-var legend = document.createElement('div');
-legend.classList.add('legend');
-document.body.appendChild(legend);
+var smalegend = document.createElement('div');
+smalegend.className = 'sma-legend';
+container.appendChild(smalegend);
 
-var firstRow = document.createElement('div');
-firstRow.innerText = 'BAGHERIA';
-firstRow.style.color = 'black';
-legend.appendChild(firstRow);
+// var legend = document.createElement('div');
+// legend.className = 'legend';
+// container.appendChild(legend);
 
-chart.subscribeCrosshairMove((param) => {
-	if (param.time) {
-		const price = param.seriesPrices.get(areaSeries);
-		firstRow.innerText = 'BAGHERIA' + '  ' + price.toFixed(2);
+
+function setMAText(smaVal) {
+	let val = 'n/a';
+	if (smaVal !== undefined) {
+		val = (Math.round(smaVal * 100) / 100).toFixed(2);
 	}
-  else {
-  	firstRow.innerText = 'BAGHERIA';
+	smalegend.innerHTML = 'MA7 <span style="color:rgba(38, 198, 218, 1)">' + val + '</span>';
+}
+
+function calculateSMA(data, count){
+  var avg = function(data) {
+    var sum = 0;
+    for (var i = 0; i < data.length; i++) {
+       sum += data[i].value;
+    }
+    return sum / data.length;
+  };
+  var result = [];
+  for (var i=count - 1, len=data.length; i < len; i++){
+    var val = avg(data.slice(i - count + 1, i));
+    const offset = 3;
+    result.push({ time: (data[i-offset].time), value: val});
   }
-});
+  return result;
+}
 
 function App() {
   return (
