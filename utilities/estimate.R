9e0a9534635edc2529731333e4ff15d2ceca3001
Gabriele Scalici <gaba41@gmail.com>
2021-03-25 23:15:43 +0100

    Update estimate.R

--- utilities/estimate.R
+++ utilities/estimate.R
@@ -23,14 +23,32 @@ log <- data.frame(Data = bag_d$time, Freq = bag_d$value)
 log$Data <- as.Date(log$Data, "%Y-%m-%d")
 df <- data.frame(I = c(log$Freq))
 
-bag_R <- estimate_R(df, method = "parametric_si", config = make_config(list(mean_si = 7.5, std_si = 3.4)))
-ER<-round(tail(bag_R$R$`Mean(R)`,1), digits = 2)
+T<- nrow(df)
+#Settimanale
+w_start <- seq(2, T-6)
+w_end <- w_start + 6
+#Bisettimanale
+bw_start <- seq(2, T-13)
+bw_end <- bw_start + 13 
+
+bag_R <- estimate_R(df, 
+                    method = "parametric_si", 
+                    config = make_config(list(
+                      t_start = bw_start,
+                      t_end = bw_end,
+                      mean_si = 7.5,
+                      std_si = 3.4))
+                    )
+
+bag_R_mean <- bag_R$R$`Mean(R)`
+
+ER<-round(tail(bag_R_mean,1), digits = 2)
 print("Stima R per "+scope+": "+ER)
 
 #GRAFICO INCIDENZA SETTIMANALE
 ggplot(data = bag_w, aes(x = time, y = value)) +
   geom_bar(stat = "identity", fill = "#878dff") +
-  labs(title = "Incidenza settimanale al COVID19 a "+scope,
+  labs(title = "Incidenza settimanale COVID19 a "+scope,
        subtitle = "Stima R: "+ER,
        x = "Data", y = "Numero nuovi positivi")
 
