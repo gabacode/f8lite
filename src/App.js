54d505c9dc21f4e453e43a81c5d26b85286c0654
Gabriele Scalici <gaba41@gmail.com>
2021-03-27 05:08:13 +0100

    date/city semi-abstraction
    looking forward

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import {Container, Row, Col, Button} from 'react-bootstrap';
+import { format } from 'date-fns';
 import Chart from './components/Chart';
 import Redzone from './components/Redzone';
 import Trend from './components/Trend';
 import Footer from './components/Footer';
 
 function App() {
+    let lastUpdate = "2021, 3, 24";
+    let cityName = "Bagheria";
   return (
     <div className="App pt--40">
       <Container>
         <Row>
           <Container>
-            <h1>Positivi Giornalieri a Bagheria</h1>
-            <h2>Aggiornato al 24/03/2021</h2>
+            <h1>Positivi Giornalieri a {cityName}</h1>
+            <h2>Aggiornato al {format(new Date(lastUpdate), 'dd/MM/yyyy')}</h2>
             <small>Fonte: ASP DISTRETTO 39</small><br/>
             <Redzone/>
           </Container>
@@ -19,7 +22,7 @@ function App() {
         <Chart />
         <Row>
             <Col xs={12} className="pt--0 ptb--30">
-              <Button size="sm" href="./datasets/1d.csv">
+              <Button size="sm" href="./datasets/1d.csv" download={`covid19-${cityName.toLowerCase()}-${format(new Date(lastUpdate), 'yyyyMMdd')}.csv`}>
                 Scarica Dataset
               </Button>
             </Col>
