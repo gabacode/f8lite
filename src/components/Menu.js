1d43b8ff212617e0526f1f1f0088a2900df17873
Gabriele Scalici <gaba41@gmail.com>
2021-03-30 14:16:51 +0200

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { FaFeatherAlt } from 'react-icons/fa';
+import { Nav, Navbar, Button } from "react-bootstrap";
+import { FaFeatherAlt, FaGithub } from 'react-icons/fa';
 
 export default class Menu extends Component{
     render(){
@@ -9,11 +9,16 @@ export default class Menu extends Component{
                 <Navbar bg="primary" variant="dark">
                     <Navbar.Brand href="/"><FaFeatherAlt/> f8lite</Navbar.Brand>
                     <Nav className="mr-auto">
-                    <Nav.Link href="/bagheria">Bagheria</Nav.Link>
-                    <Nav.Link href="/santaflavia">Santa Flavia</Nav.Link>
-                    <Nav.Link href="/ficarazzi">Ficarazzi</Nav.Link>
-                    <Nav.Link href="/casteldaccia">Casteldaccia</Nav.Link>
-                    <Nav.Link href="/altavilla">Altavilla Milicia</Nav.Link>
+                        <Nav.Link href="/bagheria">Bagheria</Nav.Link>
+                        <Nav.Link href="/santaflavia">Santa Flavia</Nav.Link>
+                        <Nav.Link href="/ficarazzi">Ficarazzi</Nav.Link>
+                        <Nav.Link href="/casteldaccia">Casteldaccia</Nav.Link>
+                        <Nav.Link href="/altavilla">Altavilla Milicia</Nav.Link>
+                    </Nav>
+                    <Nav inline>
+                        <Button size="sm" href="https://github.com/gabacode/f8lite" target="_blank" rel="noopener noreferrer">
+                            <FaGithub />
+                        </Button>
                     </Nav>
                 </Navbar>
             </Fragment>
