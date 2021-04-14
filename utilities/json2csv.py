740b4e46584687fd6216a53e1e3d945edc7a2305
Gabriele Scalici <gaba41@gmail.com>
2021-04-14 13:54:48 +0200

    Update json2csv.py
    append to main

--- utilities/json2csv.py
+++ utilities/json2csv.py
@@ -6,30 +6,25 @@ Estrai e compara i dati di oggi con quelli di ieri,
 molto instabile, attualmente indicato solo per uso interno.
 Work in progress...
 '''
-
 sys.tracebacklimit=0
 today = datetime.today()
 yesterday = today - timedelta(days=1)
 today = today.strftime('%Y%m%d')
 yesterday = yesterday.strftime('%Y%m%d')
 
-path = './dpc/dpc-covid19-ita-pa-39-'
+path = '../dati-distretto39/dpc-covid19-ita-pa-39'
 
 choice = input("Modalita' automatica? y/n\n")
 
 if choice == "n":
     dft_start = input("Inserire data di oggi\n")
-    dft = pd.read_json(path+dft_start+'.json')
+    dft = pd.read_json(path+"-"+dft_start+'.json')
     dfy_start = input("Inserire data di ieri\n")
-    dfy = pd.read_csv(path+dfy_start+'.csv')
+    dfy = pd.read_csv(path+"-"+dfy_start+'.csv')
+    #dfy = pd.read_json(path+dfy_start+'.json')
 elif choice == "y":
-    try:
-        dft = pd.read_json(path+today+'.json')
-    except:
-        print("Proviamone un altro...")
-        pass
-    finally:
-        dfy = pd.read_csv(path+yesterday+'.csv')
+    dft = pd.read_json(path+"-"+today+'.json')
+    dfy = pd.read_csv(path+"-"+yesterday+'.csv')
 else:
     print("ok...")
 
@@ -44,8 +39,9 @@ print(dft["variazione_totale_positivi"])
 
 choice = input("\nVuoi esportare? y/n\n")
 if choice == "y":
-    dft.to_csv(path+today+'.csv',index = None)
-    dft.to_csv(path+'latest.csv',index = None)
+    dft.to_csv(path+"-"+today+'.csv',index = None,float_format='%f')
+    dft.to_csv(path+'.csv', mode = 'a', index = None, header = False,float_format='%f')
+    dft.to_csv(path+"-"+'latest.csv',index = None,float_format='%f')
     print("Fatto!")
 else:
     print("Ok, magari un'altra volta, ciao!")
