99667cfb3ae6d1aec7df4e18a23246aa6c2205a9
Gabriele Scalici <gaba41@gmail.com>
2021-04-20 17:13:56 +0200

    Rpath+og

--- utilities/estimate.R
+++ utilities/estimate.R
@@ -1,10 +1,10 @@
 library(ggplot2)
 library(EpiEstim)
+suppressPackageStartupMessages()
 
-scopes <- c("bagheria","santaflavia","ficarazzi","casteldaccia","altavilla")
+scopes <- c("bagheria","santa_flavia","ficarazzi","casteldaccia","altavilla_milicia")
 options(max.print=1235813)
 
-#concat
 `+` <- function(a, b) {
   if (is.character(a) | is.character(b)) {
     paste0(a, b)
@@ -15,25 +15,26 @@ options(max.print=1235813)
 
 for(scope in scopes){
 
-#CARICAMENTO DATASET SETTIMANALE
-bag <- read.csv('../public/datasets/'+scope+'/1w_'+scope+'.csv')
-bag_d <- read.csv('../public/datasets/'+scope+'/1d_'+scope+'.csv')
-bag_w <- bag[1:(dim(bag)[1]-1),]
+#CARICAMENTO DATASET
+data_d <- read.csv('../public/datasets/1d_'+scope+'.csv')
+comune <- toString(data_d$comune[1])
 
 #STIMA DI R
-log <- data.frame(Data = bag_d$data, Freq = bag_d$nuovi_positivi)
+log <- data.frame(Data = data_d$data, Freq = data_d$nuovi_positivi)
 log$Data <- as.Date(log$Data, "%Y-%m-%d")
 df <- data.frame(I = c(log$Freq))
 
 T<- nrow(df)
+
 #Settimanale
 w_start <- seq(2, T-6)
 w_end <- w_start + 6
+
 #Bisettimanale
 bw_start <- seq(2, T-13)
 bw_end <- bw_start + 13 
 
-bag_R <- estimate_R(df, 
+data_R <- estimate_R(df, 
                     method = "parametric_si", 
                     config = make_config(list(
                       t_start = bw_start,
@@ -42,15 +43,8 @@ bag_R <- estimate_R(df,
                       std_si = 3.4))
                     )
 
-bag_R_mean <- bag_R$R$`Mean(R)`
+data_R_mean <- data_R$R$`Mean(R)`
 
-ER<-round(tail(bag_R_mean,1), digits = 2)
-print("Stima R per "+scope+": "+ER)
+ER<-round(tail(data_R_mean,1), digits = 2)
+print("Stima R per "+comune+": "+ER)
 }
-
-#GRAFICO INCIDENZA SETTIMANALE
-#ggplot(data = bag_w, aes(x = data, y = nuovi_positivi)) +
-#  geom_bar(stat = "identity", fill = "#878dff") +
-#  labs(title = "Incidenza settimanale COVID19 a "+scope,
-#       subtitle = "Stima R: "+ER,
-#       x = "Data", y = "Numero nuovi positivi")
