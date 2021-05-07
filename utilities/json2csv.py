be60b04e29a1f89f86f11f2b3a1d1224773a0540
Gabriele Scalici <gaba41@gmail.com>
2021-05-07 22:57:12 +0200

    join scripts

--- utilities/json2csv.py
+++ utilities/json2csv.py
@@ -3,9 +3,7 @@ import math
 import pandas as pd
 from datetime import datetime, timedelta
 '''
-Estrai e compara i dati di oggi con quelli di ieri,
-molto instabile, attualmente indicato solo per uso interno.
-Work in progress...
+Estrai e compara i dati di oggi con quelli di ieri
 '''
 sys.tracebacklimit=0
 today = datetime.today()
@@ -17,17 +15,14 @@ path = '../dati-distretto39/dpc-covid19-ita-pa-39'
 
 choice = input("Modalita' automatica? y/n\n")
 
-if choice == "n":
-    dft_start = input("Inserire data di oggi\n")
-    dft = pd.read_json(path+"-"+dft_start+'.json')
-    dfy_start = input("Inserire data di ieri\n")
-    dfy = pd.read_csv(path+"-"+dfy_start+'.csv')
-    #dfy = pd.read_json(path+dfy_start+'.json')
-elif choice == "y":
+if choice == "y":
     dft = pd.read_json(path+"-"+today+'.json')
     dfy = pd.read_csv(path+"-"+yesterday+'.csv')
 else:
-    print("ok...")
+    dft_start = input("Inserire data di oggi\n")
+    dft = pd.read_csv(path+"-"+dft_start+'.csv')
+    dfy_start = input("Inserire data di ieri\n")
+    dfy = pd.read_csv(path+"-"+dfy_start+'.csv')
 
 assert dft["data"][0] > dfy["data"][0], "Errore ci fu..probabilmente hai dati vecchi"
 
@@ -45,6 +40,29 @@ if choice == "y":
     dft.to_csv(path+"-"+today+'.csv',index = None)
     dft.to_csv(path+'.csv', mode = 'a', index = None, header = False)
     dft.to_csv(path+"-"+'latest.csv',index = None)
-    print("Fatto!")
 else:
-    print("Ok, magari un'altra volta, ciao!")
+    sys.exit()
+
+#Disaggrega comuni
+input = path+'.csv'
+df = pd.read_csv(input, converters={'codice_comune': '{:0>6}'.format})
+keys = ["082006","082067","082035","082023","082004"]
+
+df.rename(columns = {'distretto':'sigla_provincia'}, inplace = True)
+df['sigla_provincia'] = "PA"
+
+df['data'] = df['data'].str.slice(0,10)
+df[['lat','long']] = df[['lat','long']].round(decimals=6)
+df[['totale_ospedalizzati','isolamento_domiciliare','variazione_totale_positivi','nuovi_positivi']] = df[['totale_ospedalizzati','isolamento_domiciliare','variazione_totale_positivi','nuovi_positivi']].round().astype('Int64')
+
+comuni = df.groupby(df.codice_comune)
+output = '../dati-comuni/dpc-covid19-ita-pa-'
+
+for key in keys:
+    comune = comuni.get_group(key)
+    latest = comune.iloc[-1:]
+    latest.to_csv(output+str(key)+"-latest.csv",index=None)
+    comune.to_csv(output+str(key)+".csv",index=None)
+
+print("Fatto!")
+
