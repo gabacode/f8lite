51df2293830fd6bcba0f325a01a68ef812d2d8b1
Gabriele Scalici <gaba41@gmail.com>
2021-11-05 16:50:38 +0100

    update vaccini

--- src/components/Vax.js
+++ src/components/Vax.js
@@ -10,7 +10,7 @@ export default function Vax({istat}){
     const fetchData = useCallback(async () => {
         axios({
             "method": "GET",
-            "url": `https://raw.githubusercontent.com/opendatasicilia/informa-covid19/main/dati/vaccini/SiciliaVax-latest.csv`,
+            "url": `https://raw.githubusercontent.com/gabacode/vaxExtract/main/dati-csv/vaccini-latest.csv`,
             "responseType": "blob"
             })
             .then((response) => {
@@ -20,7 +20,8 @@ export default function Vax({istat}){
                 csvToJson()
                     .fromString(text)
                     .then((json) => {
-                        const data = json.filter( x => x.CODISTAT === istat.toString());
+                        const data = json.filter( x => parseInt(x.pro_com_t).toString() === istat.toString());
+                        console.log(data)
                         setResponseData(data)
                         setIsLoading(false)
                     })
@@ -42,8 +43,8 @@ export default function Vax({istat}){
         isLoading ? 'Caricamento...' :
         <div>
             {risultati.forEach((record) => {
-                totVax.push(parseInt(record.Vaccinati));
-                totTarget.push(parseInt(record.Target));
+                totVax.push(parseInt(record.vaccinati));
+                totTarget.push(parseInt(record.target));
             })}
             <small>Pazienti vaccinati (almeno prima dose):</small>
             <ProgressBar now={Math.round(totVax.reduce((a, b) => a + b, 0)/totTarget.reduce((a, b) => a + b, 0)*100)} />
