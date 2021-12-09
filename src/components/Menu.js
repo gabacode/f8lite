26df47398a9cadaf1dfb3703c7ab4d0a36ca8c77
Gabriele Scalici <gaba41@gmail.com>
2021-12-09 04:31:08 +0100

    larger menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { Nav, Navbar, Button } from "react-bootstrap";
+import { Nav, Navbar, Button, Container } from "react-bootstrap";
 import { FaFeatherAlt, FaGithub } from 'react-icons/fa';
 
 export default class Menu extends Component{
@@ -7,19 +7,21 @@ export default class Menu extends Component{
         return(
             <Fragment>
                 <Navbar bg="primary" variant="dark">
-                    <Navbar.Brand href="/"><FaFeatherAlt/> f8lite</Navbar.Brand>
-                    <Nav className="mr-auto">
-                        <Nav.Link className="active" href="/bagheria">Bagheria</Nav.Link>
-                        <Nav.Link className="active" href="/santaflavia">Santa Flavia</Nav.Link>
-                        <Nav.Link className="active" href="/ficarazzi">Ficarazzi</Nav.Link>
-                        <Nav.Link className="active" href="/casteldaccia">Casteldaccia</Nav.Link>
-                        <Nav.Link className="active" href="/altavilla">Altavilla Milicia</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Button size="sm" href="https://github.com/gabacode/f8lite/blob/main/README.md" target="_blank" rel="noopener noreferrer">
-                            <FaGithub />
-                        </Button>
-                    </Nav>
+                    <Container>
+                        <Navbar.Brand href="/"><FaFeatherAlt/> f8lite</Navbar.Brand>
+                        <Nav className="mr-auto">
+                            <Nav.Link className="text-white" href="/bagheria">Bagheria</Nav.Link>
+                            <Nav.Link className="text-white" href="/santaflavia">Santa Flavia</Nav.Link>
+                            <Nav.Link className="text-white" href="/ficarazzi">Ficarazzi</Nav.Link>
+                            <Nav.Link className="text-white" href="/casteldaccia">Casteldaccia</Nav.Link>
+                            <Nav.Link className="text-white" href="/altavilla">Altavilla Milicia</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Button size="sm" href="https://github.com/gabacode/f8lite/blob/main/README.md" target="_blank" rel="noopener noreferrer">
+                                <FaGithub />
+                            </Button>
+                        </Nav>
+                    </Container> 
                 </Navbar>
             </Fragment>
         )
